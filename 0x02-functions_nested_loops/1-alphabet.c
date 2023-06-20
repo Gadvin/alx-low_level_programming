ba1730bf968f2f08cc4e1d9369f873aca0dfdb4b
Gadvin <gadogechi@gmail.com>
2023-06-20 08:15:18 -0700

    Added 7 files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 97; l <= 122; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}
+
