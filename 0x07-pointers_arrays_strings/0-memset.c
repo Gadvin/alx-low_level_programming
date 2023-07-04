4822da2452b9dbe90cef2a2754e0c05d03cd13b0
Gadvin <gadogechi@gmail.com>
2023-07-04 12:13:44 -0700

    made corrections for the first file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,6 +9,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int g = 0;
+
 	for (; n > 0; g++)
 	{
 		s[g] = b;
@@ -16,4 +17,3 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
-
