1e74ed7c44a0ccb9248930ff48570ec56fbd4cf3
Gadvin <gadogechi@gmail.com>
2023-07-19 07:50:01 -0700

    added the first file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - funtion pointer to print name
+ * @name: name to output
+ * @f: pointer to function print_name
+ * return: nothing
+ **/
+
+void print_name(char *name, void (*f)(char*))
+{
+	f(name);
+}
