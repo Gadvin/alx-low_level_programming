84dac681dedb0d1f1dcea116d21b8743f2e8a99a
Gadvin <gadogechi@gmail.com>
2023-09-07 06:44:50 -0700

    update 7

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,29 +11,24 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
+	ssize_t fp;
 	char *buffer;
-	size_t bytesRead;
-	size_t bytesWritten;
+	ssize_t bytesRead;
+	ssize_t bytesWritten;
 
-	fp = fopen(filename, "r");
+	fp = open(filename, O_RDONLY);
 
-	if (fp == NULL)
+	if (fp == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	bytesRead = fread(buffer, sizeof(char), letters, fp);
+	bytesRead = read(fp, buffer, letters);
 
-	if (bytesRead == 0)
-	{
-		fclose(fp);
-		return (0);
-	}
 
-	bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 
 	free(buffer);
-	fclose(fp);
+	close(fp);
 
 	return (bytesWritten);
 }
