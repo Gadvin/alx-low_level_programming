5f05a5ab34781671c50e1ec2b64fc30c68fdf6e3
Gadvin <gadogechi@gmail.com>
2023-09-08 06:50:12 -0700

    update 8.5

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	wr = write(file, text_content, length);
 
